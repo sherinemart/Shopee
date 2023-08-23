b194afc1775c8de6ffc02d8ddb796bfcc14bf7aa
sherinemart <91425378+sherinemart@users.noreply.github.com>
2023-08-23 20:50:15 +0530

    Update Grade.java
    Second commit

--- Grade.java
+++ Grade.java
@@ -4,7 +4,7 @@ class Grade
  public static void main(String args[])
 {
  Scanner sc=new Scanner(System.in);
- System.out.println("Enter the Total: ");
+ System.out.println("Enter the Score: ");
  int total=sc.nextInt();
  if(total>=270 && total<=300)
 {
